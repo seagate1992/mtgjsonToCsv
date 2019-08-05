d1caf5aa9a2dbb9c54ad3385f1a915f96816d176
Jaton <Christophe.Jaton@yahoo.com>
2019-08-05 12:34:51 +0200

    Update main.php

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 
 include("card.php");
 
-$file=file_get_contents('AllSets.json');
+$file=file_get_contents('https://mtgjson.com/json/AllSets.json');
 $jsonDecoded=json_decode($file, true);
 
 foreach($jsonDecoded as $set){
